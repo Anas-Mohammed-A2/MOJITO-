63b7c590ba9d18edead041b6072e23275375cb61
anas mohammed <anasmohammed@MacBook-Air-anas.local>
2026-01-20 18:17:47 +0200

    first hero section code and complite it soon

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from "gsap/all"; 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 import "./index.css"
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
 const App = () => {
   return (
-    <div className='flex-center h-[100vh ] '>
-      
-      <h2 className='text-indigo-600 text-2xl'>mohammed</h2>
-    </div>
+ <main>
+  <NavBar/>
+  <Hero/>
+  <div className="h-dvh bg-black"></div>
+ </main>
   )
 }
 
