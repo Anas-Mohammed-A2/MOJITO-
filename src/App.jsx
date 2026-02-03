d2aff264a71a405dec97d695d9f9606157515119
anas mohammed <anasmohammed@MacBook-Air-anas.local>
2026-02-03 17:54:32 +0200

    adding cocktiales page and make some animation

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 import "./index.css"
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
 const App = () => {
   return (
  <main>
   <NavBar/>
   <Hero/>
+  <Cocktails/>
   <div className="h-dvh bg-black"></div>
  </main>
   )
